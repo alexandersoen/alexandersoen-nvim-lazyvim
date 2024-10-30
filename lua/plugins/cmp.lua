8c5cec32ac31485c464f2f6e53a5213d42345378
Alexander Soen <alexandersoen@gmail.com>
2024-10-30 20:07:44 +0100

    Fix cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -5,6 +5,12 @@ return {
     opts = function(_, opts)
       local cmp = require("cmp")
 
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
       -- Disable menu from appearing unless prompted
       opts.completion = {
         autocomplete = false,
@@ -30,21 +36,23 @@ return {
           fallback()
         end, { "i", "c" }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 
-        ["<C-p>"] = cmp.mapping(function()
+        ["<C-p>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          else
+          elseif has_words_before() then
             cmp.complete()
-            cmp.select_prev_item()
+          else
+            fallback()
           end
         end, { "i", "c" }),
 
-        ["<C-n>"] = cmp.mapping(function()
+        ["<C-n>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_next_item()
-          else
+          elseif has_words_before() then
             cmp.complete()
-            cmp.select_next_item()
+          else
+            fallback()
           end
         end, { "i", "c" }),
 
